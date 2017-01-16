b2a6ed782d1e679a3d0b603d05b1c6a65cff290a
Anthony Langford <anthony.langford@gmail.com>
2017-01-16 18:11:11 +0000

    added print_1337.js

--- callbacks/print_1337.js
+++ callbacks/print_1337.js
@@ -1,6 +1,12 @@
-var getHTML = require('../http-functions');
+var getHTML = require('./http-functions');
 
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step6/1337.html'
 };
+
+function print_1337(html) {
+  console.log(html.replace(/ck/g, 'x').replace(/er/g, '0r').replace(/you/g, 'j00').replace(/a/g, '4').replace(/e/g, '3').replace(/l/g, '1').replace(/o/g, '0').replace(/s/g, '5').replace(/t/g, '7'));
+}
+
+getHTML(requestOptions, print_1337);
